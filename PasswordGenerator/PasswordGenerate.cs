fcd285f037ba30b201659b0d20bc6077b9b4aadd
Abdullah AKPINAR <abdullahakpinar41@outlook.com>
2021-04-20 21:23:35 +0300

    Add project files.

---
+++ PasswordGenerator/PasswordGenerate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public static class PasswordGenerate
+    {
+        public static string CreateRandomPassword(PasswordGeneratorRequest request)
+        {
+            string validChars = string.Empty;
+            string upperChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+            string lowerChars = "abcdefghijklmnopqrstuvwxyz";
+            string numberChars = "0123456789";
+            string specialChars = "!@#$%^&*?_-";
+            if (request.IsUpperChars)
+            {
+                validChars = upperChars;
+            }
+            if (request.IsLowerChars)
+            {
+                validChars += lowerChars;
+            }
+            if (request.IsNumberChars)
+            {
+                validChars += numberChars;
+            }
+            if (request.IsSpecialChars)
+            {
+                validChars += specialChars;
+            }
+            if (!string.IsNullOrEmpty(request.AllowChars))
+            {
+                validChars += request.AllowChars;
+            }
+            if (!string.IsNullOrEmpty(request.DenyChars))
+            {
+                foreach (char itm in request.DenyChars)
+                {
+                    validChars = validChars.Remove(validChars.IndexOf(itm), 1);
+                }
+            }
+            if (string.IsNullOrEmpty(validChars))
+            {
+                validChars = upperChars + lowerChars + numberChars + specialChars;
+            }
+            Random random = new Random();
+            int size = random.Next(request.MinLength, request.MaxLength);
+            char[] chars = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGeneratorRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class PasswordGeneratorRequest
+    {
+        public PasswordGeneratorRequest()
+        {
+            MinLength = 5;
+            MaxLength = 10;
+            IsUpperChars = true;
+            IsLowerChars = true;
+            IsSpecialChars = true;
+        }
+        public int MinLength { get; set; }
+        public int MaxLength { get; set; }
+        public bool IsUpperChars { get; set; }
+        public bool IsLowerChars { get; set; }
+        public bool IsNumberChars { get; set; }
+        public bool IsSpecialChars { get; set; }
+        public string AllowChars { get; set; }
+        public string DenyChars { get; set; }
+
+
+    }
+}
